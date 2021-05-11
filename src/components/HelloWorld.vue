3dcfb53a2d0efde28e9154c598fadd8c2cea6a56
coder <942860706@qq.com>
2021-05-11 19:38:11 +0800

    首次提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,106 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="wrapper">
+    <div class="title">
+      <el-button type="primary" @click="goBack">返回</el-button>
+    </div>
+    <div class="content">
+      <div class="content_top">售币机模板设置</div>
+      <div class="content_bottom">
+        <div class="content_left"></div>
+
+        <div class="content_right">
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="页面名称">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="页面标题">
+              <el-input v-model="form.title"></el-input>
+            </el-form-item>
+            <el-form-item label="页面描述">
+              <el-input v-model="form.desc"></el-input>
+            </el-form-item>
+            <el-form-item label="客服电话">
+              <el-input v-model="form.phone"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-checkbox-group v-model="form.type">
+                <el-checkbox label="关联游戏币满减（满送）优惠" name="type"></el-checkbox>
+                <p>若勾选此选项，售币机会享受满减（满送）优惠</p>
+                <el-checkbox label="开启余币自提" name="type"></el-checkbox>
+                <p>若勾选此选项，用户可将微信余币通过售币机提取出来</p>
+                <el-checkbox label="自定义游戏币充值数量" name="type"></el-checkbox>
+                <p>勾选启用自定义购币功能，启用自定义购币不占用以下套餐数量</p>
+              </el-checkbox-group>
+            </el-form-item>
+            <el-form-item label="充值设置">
+              <p>（请设置用户充值的金额，最多可设置6项。如果是脉冲版售币机，建议单次出币不超过500币，以防出错！）</p>
+              <div class="settingItem">
+                <el-input></el-input>元
+                <el-input></el-input>币
+                <el-button>+</el-button>
+                <el-button>-</el-button>
+              </div>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      form: {
+        name: "",
+        title: "",
+        desc: "",
+        phone: "",
+        type: []
+      }
+    };
+  },
+  methods: {
+    goBack() {
+      alert("返回");
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.wrapper {
+  background-color: grey;
+  // height: 100vh;
+  height: 100%;
+  .title {
+    margin: 20px 0;
+  }
+  .content {
+    background-color: #fff;
+    margin: 20px;
+    padding:  0 0 100px 0;
+    .content_top {
+      padding: 20px;
+      border-bottom: solid 1px #ccc;
+    }
+    .content_bottom {
+      padding: 20px;
+      display: flex;
+      justify-content: center;
+
+      .content_left {
+        flex: 1;
+      }
+
+      .content_right {
+        flex: 1;
+      }
+    }
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.settingItem{
+  display: flex;
 }
 </style>
